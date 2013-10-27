075ee1b7f568dae381c3c12754332d0c04e0bb89
Radoslav Georgiev <g2h.horde@gmail.com>
2013-10-27 14:11:39 +0200

    Adding order of presentation

---
+++ order_of_presentations.md
@@ -0,0 +1,30 @@
+##  Hack For Education
+* Седем
+* Grayhat
+* Omniverbo
+* Master
+* Екип Ракета
+* Python Sucks
+* Teachbox
+* Lazy Generators
+* Edu_Real
+* The Jack
+* Master
+* Placeholder
+* Rage Quit
+* Студентите (прочетено меко)
+##  Hack For FMI
+* Powerpuff Boys
+* The Coding Monkeys
+* Коала
+* Оркестър без име
+* Зала 100
+* Mighty Mouse
+* Мотиваторите
+* Delta-Epsilon
+* HackOverflow
+* F.R.I.E.N.D.S
+* 42
+* HackOverflow
+* Гъбко
+* Морски Трепачи
--- shuffle_teams.js
+++ shuffle_teams.js
@@ -1,3 +1,23 @@
-var teams = require("./teams.json");
+var
+    teams = require("./teams.json"),
+    topics = ["Hack_For_Education", "Hack_For_FMI"],
+    _ = require("underscore"),
+    underscoreToWhite = function(str) {
+        return str.replace(new RegExp("_", "g"), " ");
+    },
+    mdItemListify = function(str) {
+        return ["*"," ", str].join("");
+    };
 
-console.log(teams);
+    topics
+        .forEach(function(item) {
+            var shuffledTeamsList = 
+                                _.shuffle(teams[item])
+                                .map(function(shuffledTeam) {
+                                    return shuffledTeam.name;
+                                })
+                                .map(mdItemListify);
+
+            console.log("## ", underscoreToWhite(item));
+            console.log(shuffledTeamsList.join("\n"));
+        });
